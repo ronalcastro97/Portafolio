3a21be9b8e7908f0ff72fce3f98c41020f61b41e
Ronal Castro <105658996+ronalcastro97@users.noreply.github.com>
2024-05-04 12:03:59 -0500

    Add files via upload

---
+++ Herenciaa/src/main/java/muestraprotected/OtraClase.java
@@ -0,0 +1,12 @@
+
+package muestraprotected;
+
+import vehiculos.Vehiculo;
+
+
+public class OtraClase {
+    public OtraClase(){
+        Vehiculo v= new Vehiculo();
+        System.out.println(v.marca);
+    }
+}
