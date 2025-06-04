8da1e418e233e1b11dac8e283e701487cf81c34b
Ronal Castro <105658996+ronalcastro97@users.noreply.github.com>
2025-06-04 10:38:41 -0500

    Delete taller-4-python-ronalcastro97 directory

--- taller-4-python-ronalcastro97/classroom/asignatura.py
+++
@@ -1,9 +0,0 @@
-class Asignatura:
-
-    def __init__(self, nombre, salon):
-        self._nombre = nombre
-        self._salon = salon
-
-    
-    def __str__(self):
-        return self._nombre()
